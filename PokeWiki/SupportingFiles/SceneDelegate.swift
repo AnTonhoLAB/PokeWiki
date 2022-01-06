5eb7a83b1b7e0dd1eb440deb5bf510894f44fe76
George Gomes <georgevag@protonmail.com>
2022-01-05 21:36:26 -0300

    Setup files organization

---
+++ PokeWiki/SupportingFiles/SceneDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  SceneDelegate.swift
+//  PokeWiki
+//
+//  Created by George Vilnei Arboite Gomes on 05/01/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        window.frame = UIScreen.main.bounds
+        
+        let viewController = ViewController()
+        
+        window.rootViewController = viewController
+        window.makeKeyAndVisible()
+        
+        self.window = window
+    }
+}
+
