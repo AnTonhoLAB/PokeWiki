728118a510dbbc1db665d5e552dd12d802b0a5ac
George Gomes <georgevag@protonmail.com>
2022-01-05 21:23:12 -0300

    Start xcode proj

---
+++ PokeWiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeWiki
+//
+//  Created by George Vilnei Arboite Gomes on 05/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
