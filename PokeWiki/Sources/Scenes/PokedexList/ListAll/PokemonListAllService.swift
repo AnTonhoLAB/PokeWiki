1a4dd00aae6fd25631d05b488437076646d60911
George Gomes <georgevag@protonmail.com>
2022-01-07 21:38:24 -0300

    Setup firs call to API

--- PokeWiki/Sources/Scenes/PokedexList/ListAll/PokemonListAllService.swift
+++ PokeWiki/Sources/Scenes/PokedexList/ListAll/PokemonListAllService.swift
@@ -7,25 +7,43 @@
 
 import Foundation
 import RxSwift
+import Alamofire
 
 protocol PokemonListAllServiceProtocol {
     func fetchAll() -> Single<PokemonListResponse>
 }
 
 final class PokemonListAllService: PokemonListAllServiceProtocol {
+        
+    private let baseURL = "https://pokeapi.co/api/v2/"
+    private let listAllEndPoint = "pokemon"
+    
     func fetchAll() -> Single<PokemonListResponse> {
         return Single<PokemonListResponse>
-            .create { single in
-                let pokemonItem = PokemonItem(name: "bulbasaur", url: "")
-                let response = PokemonListResponse(count: 1, next: "", previous: "", results: [pokemonItem])
-                single(.success(response))
+            .create { [weak self] single in
+                
+                guard let self = self else {
+                    single(.failure(PokemonListError.internalError))
+                    return Disposables.create()
+                }
+                
+                AF.request("\(self.baseURL+self.listAllEndPoint)")
+                    .responseDecodable(of: PokemonListResponse.self) { (response) in
+                        if let pokemons = response.value {
+                            single(.success(pokemons))
+                        }
+                        
+                        if let error = response.error {
+                            single(.failure(error))
+                        }
+                }
                 return Disposables.create()
             }
     }
 }
 
 struct PokemonListResponse: Decodable {
-    let count: Int
+//    let count: Int
     let next: String?
     let previous: String?
     let results: [PokemonItem]
@@ -37,5 +55,6 @@ struct PokemonItem: Decodable {
 }
 
 enum PokemonListError: Error {
+    case internalError
     case NoConnection
 }
