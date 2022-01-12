8233846451eef146661e3e5e49a7a28b93967f79
George Gomes <georgevag@protonmail.com>
2022-01-12 16:10:17 -0300

    Setup layout cell without pokemon image

--- PokeWiki/Sources/Scenes/PokedexList/Cell/PokemonListCell.swift
+++ PokeWiki/Sources/Scenes/PokedexList/Cell/PokemonListCell.swift
@@ -9,12 +9,15 @@ import UIKit
 import RxSwift
 
 class PokemonListCell: UICollectionViewCell {
-    private let nameLabel: UILabel = UILabel(frame: .zero)
     
     // MARK: - Static variables
     static let identifier = "PokemonListCell"
     
     // MARK: - Private properties
+    private let imageBG = UIImageView(image: #imageLiteral(resourceName: "BGCardNeutral").withRenderingMode(.alwaysTemplate))
+    private let imageHLBG = UIImageView(image: #imageLiteral(resourceName: "highLightBG"))
+    private let nameLabel: UILabel = UILabel(frame: .zero)
+    
     private var disposeBag = DisposeBag()
     
     // MARK: - Initializers
@@ -34,7 +37,7 @@ class PokemonListCell: UICollectionViewCell {
         viewModel.pokemonDetail.drive { [weak self] (detail) in
             guard let self = self else { return }
             self.nameLabel.text = detail.name
-            self.backgroundColor = detail.types.first?.type.name.color()
+            self.imageBG.tintColor = detail.types.first?.type.name.color()
         }.disposed(by: disposeBag)
         
         viewModel.serviceState
@@ -49,19 +52,37 @@ class PokemonListCell: UICollectionViewCell {
     
     // MARK: - Private methods
     private func setupViews() {
-        self.addSubview(self.nameLabel)
+        imageBG.addSubview(self.nameLabel)
+        addSubview(imageBG)
+        addSubview(imageHLBG)
+        nameLabel.font = UIFont(name: "GillSans-Bold", size: 17)
+        nameLabel.numberOfLines = 2
+        
+        imageBG.topAnchor.constraint(equalTo: topAnchor, constant: 20).isActive = true
+        imageBG.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        imageBG.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
+        imageBG.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10).isActive = true
+        imageBG.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageHLBG.topAnchor.constraint(equalTo: imageBG.topAnchor).isActive = true
+        imageHLBG.bottomAnchor.constraint(equalTo: imageBG.bottomAnchor, constant: -35).isActive = true
+        imageHLBG.trailingAnchor.constraint(equalTo: imageBG.trailingAnchor).isActive = true
+        imageHLBG.leadingAnchor.constraint(equalTo: imageBG.leadingAnchor, constant: 24).isActive = true
+        imageHLBG.translatesAutoresizingMaskIntoConstraints = false
+        
         nameLabel.textAlignment = .center
         nameLabel.textColor = .white
-        nameLabel.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        nameLabel.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-        nameLabel.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
-        nameLabel.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        
+        nameLabel.bottomAnchor.constraint(equalTo: imageBG.bottomAnchor, constant: -8).isActive = true
+        nameLabel.trailingAnchor.constraint(equalTo: imageBG.trailingAnchor, constant: -5).isActive = true
+        nameLabel.leadingAnchor.constraint(equalTo: imageBG.leadingAnchor, constant: 5).isActive = true
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
         self.backgroundColor = .white
+        self.imageBG.tintColor = .white
         self.nameLabel.text = ""
         disposeBag = DisposeBag()
     }
