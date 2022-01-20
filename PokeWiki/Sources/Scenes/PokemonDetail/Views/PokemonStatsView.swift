02463cf7dfb6836f6715f2b00e5fc562141c3a2b
George Gomes <georgevag@protonmail.com>
2022-01-19 22:35:50 -0300

    Add StatsView class

---
+++ PokeWiki/Sources/Scenes/PokemonDetail/Views/PokemonStatsView.swift
@@ -0,0 +1,25 @@
+//
+//  PokemonStatsView.swift
+//  PokeWiki
+//
+//  Created by George Vilnei Arboite Gomes on 19/01/22.
+//
+
+import GGDevelopmentKit
+import RxSwift
+
+class PokemonStatsView: UIView, ViewCoded {
+
+
+    func setupViews() {
+        // TODO: - setupViews
+    }
+    
+    func setupViewConfigs() {
+        // TODO: - setupViewConfigs
+    }
+    
+    func setupConstraints() {
+        // TODO: - setupConstraints
+    }
+}
